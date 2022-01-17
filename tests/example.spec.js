6dc0f0d5d308d4e7ab55159a730360d769c43550
Jonathan Gober <gober.jonathan@gmail.com>
2022-01-17 08:49:49 -0500

    randomized starter pokemon

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,10 @@
-// @ts-check
 const { test } = require('@playwright/test');
+let starterPokemon = [1, 4, 7, 25, 133, 152, 155, 158, 252, 255, 258, 387, 390, 393]
+let randomStarterPokemon = starterPokemon[Math.floor(Math.random() * starterPokemon.length)];
+
 test('Whose That Pokemon?!?!?', async ({ request }) => {
-  const pokemon = await request.get(`pokemon/7`);
+  const pokemon = await request.get(`pokemon/${randomStarterPokemon}`);
+  console.log(randomStarterPokemon);
   let myStarterPokemon = await pokemon.json()
   console.log(myStarterPokemon.name)
 });
